d57bfa56e15123bc193bb5739016ffdf6791f087
Ricardo Miranda <mail@ricardoMiranda>
2019-12-25 14:25:35 +0000

    Checkout refactored

--- src/main/scala/com/ricardomiranda/shop/Checkout.scala
+++ src/main/scala/com/ricardomiranda/shop/Checkout.scala
@@ -1,13 +1,49 @@
 package com.ricardomiranda.shop
 
 import com.typesafe.scalalogging.StrictLogging
-import com.ricardomiranda.shop.ProductsOnSale._
+import spray.json._
+import DefaultJsonProtocol._
 
 
-case class Checkout(products: Products) extends StrictLogging 
+case class Products(products: Seq[Product])
+case class Product(code: String, name: String, price: Double) 
 
-object Checkout {
-  def apply(configurationFilePath: String): Checkout = {
-    new Checkout(products = ProductsOnSale.convertConfigFileContentsToObject(configurationFilePath = configurationFilePath))
+
+case class Checkout(products: Products)
+
+object Checkout extends StrictLogging {
+  def apply(pricing_rules: String): Checkout = {
+    new Checkout(products = this.convertConfigFileContentsToObject(configurationFilePath = pricing_rules))
   }
+
+  implicit val productFormat: RootJsonFormat[Product] = jsonFormat3(Product)
+  implicit val productsFormat: RootJsonFormat[Products] = jsonFormat1(Products)
+
+  /** Method to convert the contents of a configuration file into an object of type Products
+    *
+    * @param configurationFilePath The file path of the configuration.
+    * @return Some(Products) if the file path exists or None if not.
+    */
+  def convertConfigFileContentsToObject(configurationFilePath: String): Products = {
+    logger.info(s"Converting config file - $configurationFilePath - to Products object")
+    Core.getFileContents(filePath = configurationFilePath)
+      .get
+      .parseJson
+      .convertTo[Products]
+  }
+
+  /** Method to create a Set[String] of the products on the Triggerise's shop
+   * 
+   * @param products the Seq of products on Triggerise's shop
+   * @return Set[String] whith the code of products
+   */
+  def availableProducts(products: Seq[Product]): Set[String] = products.map(_.code.trim.toUpperCase).toSet
+
+  /** Method that validates if a product is available on Triggerise's shop
+   * 
+   * @param product Code of product to be checked
+   * @param products the Set[String] of products on Triggerise's shop
+   * @return Boolean True if product is available for sale
+   */
+  def isValidProduct(product: String, products: Set[String]): Boolean = products.contains(product.trim.toUpperCase)
 }
--- src/test/scala/com/ricardomiranda/shop/CheckoutSpec.scala
+++ src/test/scala/com/ricardomiranda/shop/CheckoutSpec.scala
@@ -1,10 +1,9 @@
 package com.ricardomiranda.shop
 
 import org.scalatest.{Matchers, WordSpec}
-import com.ricardomiranda.shop.ProductsOnSale.{Product, Products}
 
 
-class ProductsOnSaleSpec extends WordSpec with Matchers {
+class CheckoutSpec extends WordSpec with Matchers {
   val testProductsEmpty: Products = Products(
     products = Seq()
   )
@@ -56,7 +55,7 @@ class ProductsOnSaleSpec extends WordSpec with Matchers {
 
   "convertConfigFileContentsToObject" should {
     "convert an existing json config file with no products to Products object with no products" in {
-      val actual: Products = ProductsOnSale.convertConfigFileContentsToObject(
+      val actual: Products = Checkout.convertConfigFileContentsToObject(
         configurationFilePath = getClass.getResource("/products_empty.json").getPath)
 
       val expected: Products = testProductsEmpty
@@ -64,7 +63,7 @@ class ProductsOnSaleSpec extends WordSpec with Matchers {
     }
 
     "convert an existing json config file with 1 product to Products object with 1 product" in {
-      val actual: Products = ProductsOnSale.convertConfigFileContentsToObject(
+      val actual: Products = Checkout.convertConfigFileContentsToObject(
         configurationFilePath = getClass.getResource("/products_1.json").getPath)
 
       val expected: Products = testProducts1
@@ -72,7 +71,7 @@ class ProductsOnSaleSpec extends WordSpec with Matchers {
     }
 
     "convert an existing json config file with 2 products to Products object with 2 products" in {
-      val actual: Products = ProductsOnSale.convertConfigFileContentsToObject(
+      val actual: Products = Checkout.convertConfigFileContentsToObject(
         configurationFilePath = getClass.getResource("/products_2.json").getPath)
 
       val expected: Products = testProducts2
@@ -80,7 +79,7 @@ class ProductsOnSaleSpec extends WordSpec with Matchers {
     }
 
     "convert an existing json config file with 3 products to Products object with 3 products" in {
-      val actual: Products = ProductsOnSale.convertConfigFileContentsToObject(
+      val actual: Products = Checkout.convertConfigFileContentsToObject(
         configurationFilePath = getClass.getResource("/products_3.json").getPath)
 
       val expected: Products = testProducts3
@@ -90,24 +89,54 @@ class ProductsOnSaleSpec extends WordSpec with Matchers {
 
   "availableProducts" should {
     "create an empty set with a empty sequence of Products" in {
-      val actual: Set[String] = ProductsOnSale.availableProducts(products = testProductsEmpty.products)
+      val actual: Set[String] = Checkout.availableProducts(products = testProductsEmpty.products)
 
       val expected: Set[String] = Set()
       assert(expected.equals(actual))
     }
 
     "create an sequence with 1 element from a unitary sequence of Products" in {
-      val actual: Set[String] = ProductsOnSale.availableProducts(products = testProducts1.products)
+      val actual: Set[String] = Checkout.availableProducts(products = testProducts1.products)
 
       val expected: Set[String] = Set("TICKET")
       assert(expected.equals(actual))
     }
 
     "create an sequence with 3 elements from a sequence of 3 Products" in {
-      val actual: Set[String] = ProductsOnSale.availableProducts(products = testProducts3.products)
+      val actual: Set[String] = Checkout.availableProducts(products = testProducts3.products)
 
       val expected: Set[String] = Set("TICKET", "HAT", "HOODIE")
       assert(expected.equals(actual))
     }
   }
+
+   "validProduct" should {
+     "Be false if both empty" in {
+       val product: String = ""
+       val validProducts: Set[String] =  Checkout.availableProducts(products = testProductsEmpty.products)
+       val actual: Boolean =  Checkout.isValidProduct(product = product, products = validProducts)
+       assert(!actual)
+     }
+
+     "Be false if Triggerise's shop has no products" in {
+       val product: String = "HAT"
+       val validProducts: Set[String] =  Checkout.availableProducts(products = testProductsEmpty.products)
+       val actual: Boolean =  Checkout.isValidProduct(product = product, products = validProducts)
+       assert(!actual)       
+     }
+
+     "Be false if Triggerise's shop has other products" in {
+       val product: String = "HAT"
+       val validProducts: Set[String] =  Checkout.availableProducts(products = testProducts1.products)
+       val actual: Boolean =  Checkout.isValidProduct(product = product, products = validProducts)
+       assert(!actual)       
+     }
+
+     "Be true if Triggerise's shop has the product" in {
+       val product: String = "HAT"
+       val validProducts: Set[String] =  Checkout.availableProducts(products = testProducts3.products)
+       val actual: Boolean =  Checkout.isValidProduct(product = product, products = validProducts)
+       assert(actual)       
+     }
+   }
 }
