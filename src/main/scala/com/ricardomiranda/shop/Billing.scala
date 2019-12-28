9128ad5236e478da4d35b056a40fa577fd3f93c5
Ricardo Miranda <mail@ricardoMiranda>
2019-12-28 15:35:50 +0000

    Billing 2-for-1 promotion

--- src/main/scala/com/ricardomiranda/shop/Billing.scala
+++ src/main/scala/com/ricardomiranda/shop/Billing.scala
@@ -20,6 +20,16 @@ case class Regular(code: String) extends Billing with StrictLogging {
   }
 }
 
+case class TwoForOne(code: String) extends Billing with StrictLogging {
+
+  override def computeBill: (Double, Long) => Double = (quantity, price) => {
+    val subtotal: Double = (quantity / 2) * price
+    logger.info(s"Subtotal for code ${code} is ${subtotal%2.2f} Euro (${quantity} units at ${price%2.2f} Euro)")
+    subtotal
+  }
+}
+
+
 object BillingFactory extends StrictLogging {
 
   /** Method that dynamically creates an object of the intended Billing type
@@ -33,6 +43,9 @@ object BillingFactory extends StrictLogging {
       case "regular" =>
         logger.info(s"Code ${code} will be billed without any promotion")
         Regular(code = code.trim.toUpperCase)
+      case "two_for_one" =>
+        logger.info(s"Code ${code} will be billed 2-for-1 promotion")
+        TwoForOne(code = code.trim.toUpperCase)
       case _ =>
         logger.error(s"Code ${code} does not have a known billing type")
         sys.exit(1)
